1a9a1951168711f0e2980b0660a0a353f27d4bfa
Tanvir Rubayed <tanvirrubayedtauhid@gmail.com>
2021-02-28 05:00:53 +0600

    how to use props on react when passing dynamic data

--- src/App.js
+++ src/App.js
@@ -2,24 +2,26 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit done 2222  <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Person name="Munna" job="Footballer"></Person>
+        <Person name="Masud" job="Dorshok"></Person>
       </header>
     </div>
   );
 }
-
+function Person(Props){
+  let personStyle={
+    border: '5px solid red',
+    margin: "10px"
+  }
+  return (
+    <div style={personStyle}>
+      <h3>My name: {Props.name}</h3>
+      <p>My Profession is {Props.job}</p>
+    </div>
+  )
+}
 export default App;
